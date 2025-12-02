77f3d3d7dd510e3fdaed569e41ac2ad33ff62f84
Just-try-it03 <xionghui_fan@163.com>
2025-12-02 11:09:46 +0800

    删除build

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mm/dist_ackermann
-# Build directory: /home/mm/dist_ackermann/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
